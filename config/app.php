46bf8c80aea2b693daf7aa85514e22b206618555
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-01-03 03:32:56 +0200

    theme1 service provider included.

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        D3VSOFT\Theme1\Providers\Theme1ServiceProvider::class,
 
     ],
 
