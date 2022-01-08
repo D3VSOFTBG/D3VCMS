981b67856910f14171fc854f59e095f2a6413f8f
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-01-08 13:15:43 +0200

    user_edit added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -27,6 +28,51 @@ class AdminController extends Controller
     function user_delete(Request $request)
     {
         User::findOrFail($request->id)->delete();
+        return back();
+    }
+    function user_edit(Request $request)
+    {
+        $user = User::findOrFail($request->id);
+
+        $request->validate([
+            'name' => 'required',
+            'role' => 'required|integer',
+        ]);
+
+        $user->name = $request->name;
+
+        if($request->email != $user->email)
+        {
+            $request->validate([
+                'email' => 'required|email|unique:users',
+            ]);
+            $user->email = $request->email;
+        }
+
+        if($request->role == 0)
+        {
+            $user->role = NULL;
+        }
+        else
+        {
+            Role::findOrFail($request->role);
+            $user->role = $request->role;
+        }
+
+        if(!empty($request->password) && !empty($request->password_confirmation))
+        {
+            if($request->password == $request->password_confirmation)
+            {
+                $user->password = Hash::make($request->password);
+            }
+            else
+            {
+                return back()->withErrors('The passwords do not match.');
+            }
+        }
+
+        $user->save();
+
         return back();
     }
 }
