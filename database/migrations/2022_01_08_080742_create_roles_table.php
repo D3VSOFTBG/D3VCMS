5ca891cd55f68c122db2cdb76c22a8dc6aafe00a
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-01-08 10:27:15 +0200

    role updated

--- database/migrations/2022_01_08_080742_create_roles_table.php
+++ database/migrations/2022_01_08_080742_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
