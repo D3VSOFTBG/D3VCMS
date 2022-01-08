230709a616955ede4790a6b50906ab01770e432d
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-01-08 12:31:35 +0200

    UserSeeder updated

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +12,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'D3VSOFT',
+            'email' => 'admin@d3vsoft.com',
+            'password' => '$2y$10$WEf3oHnfdXCjHFZ3yVFvlO7BiohpAtyTqH2wQpbIfeOYCee0lZ6Rq',
+            'role' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
