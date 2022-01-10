78da305b5c4711d6290deec885a5d867af044404
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-01-10 21:16:10 +0200

    laravel batch installed

--- config/app.php
+++ config/app.php
@@ -165,6 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
+        D3VSOFT\Main\Providers\MainServiceProvider::class,
+        Mavinoo\Batch\BatchServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -174,7 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        D3VSOFT\Main\Providers\MainServiceProvider::class,
 
     ],
 
@@ -226,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Batch' => Mavinoo\Batch\BatchFacade::class,
 
     ],
 
