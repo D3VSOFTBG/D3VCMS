ca0f8e882bd74ebabe7fb24341f824b29ea9b0d8
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-01-15 13:11:01 +0200

    env to setting

--- packages/D3VSOFT/Main/src/resources/views/inc/header.blade.php
+++ packages/D3VSOFT/Main/src/resources/views/inc/header.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{setting('TITLE')}} {{setting('TITLE_SEPERATOR')}} @yield('page_name')</title>
-    <link rel="icon" type="image/x-icon" href="{{asset('/storage/img/global/' . env('FAVICON'))}}">
+    <link rel="icon" type="image/x-icon" href="{{asset('/storage/img/global/' . setting('FAVICON'))}}">
     <!-- Bootstrap -->
     <link href="{{asset("$theme_path/assets/css/bootstrap.min.css")}}" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- D3VSOFT -->
--- resources/views/auth/inc/header.blade.php
+++ resources/views/auth/inc/header.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{setting('TITLE')}} {{setting('TITLE_SEPERATOR')}} @yield('page_name')</title>
-    <link rel="icon" type="image/x-icon" href="{{asset('/storage/img/global/' . env('FAVICON'))}}">
+    <link rel="icon" type="image/x-icon" href="{{asset('/storage/img/global/' . setting('FAVICON'))}}">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
