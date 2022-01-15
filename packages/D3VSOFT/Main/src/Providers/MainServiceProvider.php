a3d316ddb92aa287703ec8a956fedbbb718460eb
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-01-15 12:32:48 +0200

    publishes for migrations & factories & seeds

--- packages/D3VSOFT/Main/src/Providers/MainServiceProvider.php
+++ packages/D3VSOFT/Main/src/Providers/MainServiceProvider.php
@@ -29,6 +29,9 @@ class MainServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../public' => public_path('/d3vsoft/main'),
+            __DIR__.'/../database/seeds' => database_path('seeds'),
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+            __DIR__.'/../database/factories' => database_path('factories'),
         ]);
 
         $theme_path = '/d3vsoft/main';
